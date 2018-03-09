97cb2fe401df0de9d3cc07839a339769745e1f9d
JoshuaMa <majun945@gmail.com>
2018-03-09 19:00:34 +0800

    添加项目文件。

---
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            var list = new List<Student>
+		    {
+		        new Student("Jotaro", 3),
+		        new Student("Giorno", 5),
+		        new Student("Kakyoin", 3),
+		        new Student("Josuke", 4)
+		    };
+
+		    ListView.ItemsSource = list;
+
+		    ListView.BindingContext = list;
+
+		}
+	}
+}
---
+++ XamarinApp/XamarinApp/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp
+{
+    class Student
+    {
+        public Student(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        private string Name { get; set; }
+        private int Score { get; set; }
+    }
+}
